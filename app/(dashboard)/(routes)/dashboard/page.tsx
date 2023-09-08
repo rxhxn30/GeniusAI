6d5aabe15045093126ed13eaadb8481424003970
Rohan Janardhan <rohanjanardhan30@gmail.com>
2023-09-08 18:40:29 +0530

    Setting up Auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
     return(
         <div>
-            Dashboard Page (Protected)
+            <p>Dashboard Page (Protected)</p>
+            <UserButton afterSignOutUrl="/" />
         </div>
     );
 }
